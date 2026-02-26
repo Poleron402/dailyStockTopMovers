e21e9a4b89fc685617f0f7a48ec96a660a9875db
Pol <zhperon@gmail.com>
2026-02-25 20:07:20 -0800

    Simple frontend code done

---
+++ fe/src/App.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { SpinnerRoundFilled } from 'spinners-react';
+
+interface Stock {
+  percent_change: number,
+  date: string,
+  ticker: string,
+  closing_price: number
+}
+function App() {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [data, setData] = useState<Stock[] | undefined>()
+  const [error, setError] = useState<string | undefined>()
+
+  useEffect(()=>{
+    const getData = async() =>{
+      try {
+        const result = await axios.get("https://zabvgvh4j7.execute-api.us-west-1.amazonaws.com/prod/movers")
+        if (result) {
+          setLoading(false)
+          setData(result.data)
+        }
+      }catch(e) {
+        setError("There has been an error with fetching information.")
+        console.log(`There has been an error calling the API - ${e}`)
+      }
+    }
+
+    getData()
+  }, [])
+  return (
+    <>
+      {!error && (
+        <>
+        <h1>Stock Top-Movers</h1>
+        <p>Data for the last 7 business days</p>
+        {!loading && data ?
+        (
+          <table>
+            <tr>
+              <th>Date</th>
+              <th>Ticker</th>
+              <th>Closing Price</th>
+              <th>% Change</th>
+            </tr>
+            { data.map((item, id)=>(
+              <tr key={id} className={item.percent_change>0? "positive_row" : "negative_row"}>
+                <td>{item.date}</td>
+                <td>{item.ticker}</td>
+                <td>${item.closing_price}</td>
+                <td>{item.percent_change.toFixed(2)}%</td>
+              </tr>
+            ))
+            }
+          </table>
+      ):
+      <SpinnerRoundFilled size="200px" enabled={true} color="#1E90FF"/>
+      }
+      </>
+      )}
+      
+    </>
+  )
+}
+
+export default App
